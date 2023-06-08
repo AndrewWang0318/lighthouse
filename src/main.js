747a4b65209da7d8496d892491ce50001f91e157
WangNing <AndrewWang0318@outlook.com>
2023-06-08 18:51:06 +0800

    添加插件,删除无关页面

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Pixelfit from 'pixelfit'
+
 const app = createApp(App)
+const pixelfit = new Pixelfit({window,width:750});
+pixelfit.pixelfit();
+app.config.globalProperties.pixelfit = pixelfit;
 
 app.use(createPinia())
 app.use(router)
