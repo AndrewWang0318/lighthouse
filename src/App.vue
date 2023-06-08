cc84d70b0e6b61b383169c87e752df4fc4b30c2e
WangNing <AndrewWang0318@outlook.com>
2023-06-08 15:41:21 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+import { Button } from 'vant';
+</script>
+
+<template>
+  <div class="btn">
+    
+    <Button type="primary">主要按钮</Button>
+  </div>
+  <HelloWorld msg="组件引入示例" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>{{ count }}</h2>
+  <div class="card">
+    <button type="button" @click="count++">增加</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
