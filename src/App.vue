8df748dc6125138784fc0e74b9ba7e7899c965d6
WangNing <AndrewWang0318@outlook.com>
2023-06-08 19:03:13 +0800

    页面调整

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <HelloWorld />
-  </div>
   <RouterView />
 </template>
 
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    登录页
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
