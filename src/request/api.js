e5e92d15569c379bbc5aef8716bd24210cf0bb3b
WangNing <AndrewWang0318@outlook.com>
2023-06-15 15:45:35 +0800

    添加接口

--- src/request/api.js
+++ src/request/api.js
@@ -26,8 +26,28 @@ class Api extends Axios {// Api继承Axios
   async login(parmas = {}) { // 登录
     return await this.axios("POST", "/user/login", parmas);
   }
+  async updateUserInfo(parmas = {}) { // 修改基本信息接口
+    return await this.axios("POST", "/user/updateUserInfo", parmas);
+  }
+  async updateUserAvatar(parmas = {}) { // 修改头像接口
+    return await this.axios("POST", "/user/updateUserAvatar", parmas);
+  }
+  async updateUserEmail(parmas = {}) { // 修改密码
+    return await this.axios("POST", "/user/updateUserEmail", parmas);
+  }
+  async sendEmailCode(parmas = {}) { // 发送邮箱验证码
+    return await this.axios("POST", "/user/sendEmailCode", parmas);
+  }
+  async verifyEmailCode(parmas = {}) { // 检查邮箱验证码
+    return await this.axios("POST", "/user/verifyEmailCode", parmas);
+  }
+
+
+
+
+
   async dynamicList(parmas = {}){ // 获取动态列表
-    return await this.axios("POST", "/user/login", parmas);
+    return await this.axios("POST", "/dynamic/getDynamicList", parmas);
   }
 }
 
