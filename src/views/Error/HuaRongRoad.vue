53ee42958012a973f4d1c19a6742bee043846987
Wang Ning <AndrewWang0318@outlook.com>
2023-07-01 18:11:40 +0800

    华容道游戏逻辑编写完成

--- src/views/Error/HuaRongRoad.vue
+++ src/views/Error/HuaRongRoad.vue
@@ -1,41 +1,41 @@
 <template >
   <div class="page-hrd">
     <div id="game-title">华容道</div>
+    <div>移动步数{{ move_step }}</div>
+    <div>规则</div>
     <div id="game-content">
       <div id="role-content">
-        <div 
-          v-for="v in all_role"
-          :class="`role-item ${v.name}`"
-          :style="{
-            width:v.width,
-            height:v.height,
-            top:`${v.top}rem`,
-            left:`${v.left}rem`,
-            position: 'absolute',
-            backgroundImage: `url( ${ getImage(`./images/hrd/${v.img}`) } )`,
-            border: '0.02rem solid gray',
-            boxSizing:'border-box',
-          }"
-          v-touch:swipe.left="()=>{swipeHandler('l',v)}"
-          v-touch:swipe.right="()=>{swipeHandler('r',v)}"
-          v-touch:swipe.top="()=>{swipeHandler('t',v)}"
-          v-touch:swipe.bottom="()=>{swipeHandler('b',v)}"
-        >{{ v.cn_name }}</div>
+        <div v-for="v in all_role" :class="`role-item ${v.name}`" :style="{
+          width: v.width,
+          height: v.height,
+          top: `${v.top}rem`,
+          left: `${v.left}rem`,
+          position: 'absolute',
+          backgroundImage: `url( ${getImage(`/assets/images/hrd/${v.img}`)} )`,
+          border: '0.02rem solid gray',
+          boxSizing: 'border-box',
+        }" v-touch:swipe.left="() => { touchMove('l', v) }" v-touch:swipe.right="() => { touchMove('r', v) }"
+          v-touch:swipe.top="() => { touchMove('t', v) }" v-touch:swipe.bottom="() => { touchMove('b', v) }">{{
+            v.cn_name }}
+        </div>
       </div>
       <div id="export"></div>
     </div>
+
+    <div>排行榜</div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
-
+import all_level_data from "./hrd_level"
+import { ref } from 'vue';
 const grid_size = 1.5;// 布局一格的长度
 const wall_w = 4; // 盒子的宽
 const wall_h = 5; // 盒子的高
 const export_w = 2; // 出口的宽度
+let move_step = ref(0);
 class Soldier {
-  constructor(w,h,lt,img_name,name,cn_name) {
+  constructor(w, h, lt, img_name, name, cn_name) {
     this.class_name = name;
     this.cn_name = cn_name;
     this.width = `${w * grid_size}rem`;
@@ -43,165 +43,158 @@ class Soldier {
     this.top = lt[1] * grid_size;
     this.left = lt[0] * grid_size;
     this.img = img_name;
-
     // 出口坐标
     this.export_locate = {
-      rb:{ x:(wall_w - export_w) / 2,y:wall_h },
-      lb:{ x:((wall_w - export_w) / 2 ) + export_w,y:wall_h }
+      rb: { x: (wall_w - export_w) / 2, y: wall_h },
+      lb: { x: ((wall_w - export_w) / 2) + export_w, y: wall_h }
     }
     // 墙壁坐标
     this.wall_locate = {
-      lt:{x:0,y:0},
-      rt:{x:wall_w,y:0},
-      rb:{x:wall_w,y:wall_h},
-      lb:{x:0,y:wall_h}
+      lt: { x: 0, y: 0 },
+      rt: { x: wall_w, y: 0 },
+      rb: { x: wall_w, y: wall_h },
+      lb: { x: 0, y: wall_h }
     }
     // 角色坐标
     this.role_locate = {
-      lt:{ x:lt[0],y:lt[1] },
-      rt:{ x:lt[0]+w,y:lt[1] },
-      rb:{ x:lt[0]+w,y:lt[1] + h },
-      lb:{ x:lt[0],y:lt[1] + h },
-      name:name
+      lt: { x: lt[0], y: lt[1] },
+      rt: { x: lt[0] + w, y: lt[1] },
+      rb: { x: lt[0] + w, y: lt[1] + h },
+      lb: { x: lt[0], y: lt[1] + h },
+      name: name
     }
   }
-  itCanMove(all_locate,move_direct) { // 是否可以移动 move_way移动方式[前进或后退] move_direct移动方向
-    let dir = ''
-    let start_opint = ''
-    let end_opint = ''
-    if(move_direct == 'l'){
-      dir = 'x'
-      start_opint = 'lt'
-      end_opint = 'lb'
-    }
-    if(move_direct == 'r'){
-      dir = 'x'
-      start_opint = 'rt'
-      end_opint = 'rb'
-    }
-    if(move_direct == 't'){
-      dir = 'y'
-      start_opint = 'lt'
-      end_opint = 'rt'
-    }
-    if(move_direct == 'b'){
-      dir = 'y'
-      start_opint = 'lb'
-      end_opint = 'rb'
-    }
-
-    
-
-    let is_block_wall = false; // 是否有墙阻挡
-    let is_block_other = false; // 是否有其他角色阻挡
-
-    let wall_start_point_close = this.isClose(this.role_locate[start_opint],this.wall_locate[start_opint],this.wall_locate[end_opint])
-    let wall_end_point_close = this.isClose(this.role_locate[end_opint],this.wall_locate[start_opint],this.wall_locate[end_opint])
-    if(!wall_start_point_close && !wall_end_point_close) is_block_wall = true;
-    // 如果是曹操在出口则不算墙阻挡
-    if(this.name == 'cc'&& move_direct == 'b' && this.role_locate.lb.x == this.export_locate.lb.x && this.role_locate.rb.x == this.export_locate.rb.x){
-      is_block_wall = false;
-    }
+  itCanMove(all_locate, move_direct) { // 是否可以移动 move_way移动方式[前进或后退] move_direct移动方向
+    // 是否有其他角色阻挡
+    let is_block_other = false;
     all_locate.forEach( other_local => {
-      // 如果当前角色的 (左上点的y >= .左上点的y && 左上点的y < 坐下点y) && 左上点的x == .右上点的x
-      // 判断两点坐标 在目标两点之中
-      let role_start_point_close = this.isClose(this.role_locate[start_opint],other_local[start_opint],other_local[end_opint])
-      let role_end_point_close = this.isClose(this.role_locate[end_opint],other_local[start_opint],other_local[end_opint])
-      if(role_start_point_close && role_end_point_close) is_block_other = true;
+      let role_close = this.isCloseRole(move_direct,this.role_locate,other_local)
+      if(role_close) is_block_other = true;
     })
-    if(is_block_wall){
+    // 是否有墙阻挡
+    let is_block_wall = this.isCloseWall(move_direct, this.role_locate);
+    if (is_block_wall) {
       console.log('有墙壁阻挡');
       return false;
-    }else if(is_block_other){
+    } else if (is_block_other) {
       console.log('有人阻挡');
       return false;
-    }else{
+    } else {
       return true;
     }
   }
-  isClose(currPoint,point1,point2){
-    let x_pass = Math.abs(point1.x) <= Math.abs(currPoint.x) <= Math.abs(point2.x);
-    let y_pass = Math.abs(point1.y) <= Math.abs(currPoint.y) <= Math.abs(point2.y);
+  isCloseRole(move_direct, cur_local, tar_local) { // 是否靠近其他角色
+    let c_dir,t_dir; // 接触方向 判断方向
+    let c_p1, c_p2, t_p1, t_p2;
+    if (move_direct == 'l') {
+      c_p1 = cur_local.lt
+      c_p2 = cur_local.lb
+      t_p1 = tar_local.rt
+      t_p2 = tar_local.rb
 
-    return x_pass && y_pass
+      c_dir = 'x'
+      t_dir = 'y'
+    }
+    if (move_direct == 'r') {
+      c_p1 = cur_local.rt
+      c_p2 = cur_local.rb
+      t_p1 = tar_local.lt
+      t_p2 = tar_local.lb
+
+      c_dir = 'x'
+      t_dir = 'y'
+    }
+    if (move_direct == 't') {
+      c_p1 = cur_local.lt
+      c_p2 = cur_local.rt
+      t_p1 = tar_local.lb
+      t_p2 = tar_local.rb
+
+      c_dir = 'y'
+      t_dir = 'x'
+    }
+    if (move_direct == 'b') {
+      c_p1 = cur_local.lb
+      c_p2 = cur_local.rb
+      t_p1 = tar_local.lt
+      t_p2 = tar_local.rt
+
+      c_dir = 'y'
+      t_dir = 'x'
+    }
+    // 当前角色的起始点或结束点 处于 目标贴合边起始点和结束点之间 则存在阻挡
+    let is_inset1 = (c_p1[c_dir] == t_p1[c_dir]) && (c_p1[t_dir] > t_p1[t_dir] && c_p1[t_dir] < t_p2[t_dir])
+    let is_inset2 = (c_p2[c_dir] == t_p1[c_dir]) && (c_p2[t_dir] > t_p1[t_dir] && c_p2[t_dir] < t_p2[t_dir])
+    let is_inset = is_inset1 || is_inset2
+    // 当前角色的起始点或结束点 与 目标贴合边起始点或结束点有重合 则存在阻挡
+    let is_close1 = c_p1.x == t_p1.x && c_p1.y == t_p1.y
+    let is_close2 = c_p2.x == t_p2.x && c_p2.y == t_p2.y
+    let is_close = is_close1 || is_close2
+
+    return is_close || is_inset;
   }
-  isWin(cc_locate){ // 是否胜利
+  isCloseWall(move_direct,cur_local){ // 是否靠近墙壁
+    let is_close_wall = false
+    if (move_direct == 'l') is_close_wall = cur_local.lt.x == this.wall_locate.lt.x;
+    if (move_direct == 'r') is_close_wall = cur_local.rt.x == this.wall_locate.rt.x;
+    if (move_direct == 't') is_close_wall = cur_local.lt.y == this.wall_locate.lt.y;
+    if (move_direct == 'b') is_close_wall = cur_local.rb.y == this.wall_locate.rb.y;
+    if(cur_local.name == 'cc' && move_direct == 'b' &&  cur_local.lb.x == this.export_locate.lb.x && cur_local.rb.x == this.export_locate.rb.x){
+      is_close_wall = false;
+    }
+    return is_close_wall
+  }
+  isWin(cc_locate) { // 是否胜利
     return cc_locate.lb.y > this.export_locate.lb.y
   }
 
-  move(move_direct,step = 1){ // 坐标改变
-    let dir = 'x';
+  move(move_direct, step = 1) { // 坐标改变
     let distence = 1
-    if(move_direct == 'l'){
-      dir = 'x';
+    if (move_direct == 'l' || move_direct == 't') {
       distence = -1;
     }
-    if(move_direct == 'r'){
-      dir = 'x';
+    if (move_direct == 'r' || move_direct == 'b') {
       distence = 1;
     }
-    if(move_direct == 't'){
-      dir = 'y';
-      distence = -1;
-    }
-    if(move_direct == 'b'){
-      dir = 'y';
-      distence = 1;
-    }
-    Object.keys(this.role_locate).forEach( key => {
-      this.role_locate[key][dir] += distence
-      console.log(this.role_locate[key][dir])
+    Object.keys(this.role_locate).forEach(key => {
+      if (key !== 'name') {
+        const item = this.role_locate[key]
+        if (move_direct == 'b' || move_direct == 't') {
+          item.y += distence
+        } else {
+          item.x += distence
+        }
+      }
     })
   }
 }
-let cc = new Soldier(2,2,[1,0],'cc.png','cc','曹操');
-let hz = new Soldier(1,2,[0,0],'hz.png','hz','黄忠');
-let mc = new Soldier(1,2,[3,0],'mc.png','mc','马超');
-let zf = new Soldier(1,2,[0,2],'zf.png','zf','张飞');
-let gy = new Soldier(2,1,[1,2],'gy.png','gy','关羽');
-let zy = new Soldier(1,2,[3,2],'zy.png','zy','赵云');
 
-
-
-let xb1 = new Soldier(1,1,[0,4],'xb.png','xb1','士');
-let xb2 = new Soldier(1,1,[1,3],'xb.png','xb2','士');
-let xb3 = new Soldier(1,1,[2,3],'xb.png','xb3','士');
-let xb4 = new Soldier(1,1,[3,4],'xb.png','xb4','士');
-
-let all_role = ref([ cc,gy,zf,zy,mc,hz,xb1,xb2,xb3,xb4 ]); // 所有人物
-
-let __all_role_locate = all_role.value.map( v => v.role_locate ); // 所有人物坐标点
-
-let all_role_locate = ref(__all_role_locate)
-
-function getImage(url){
-  return new URL(url, import.meta.url).href
+let all_role = ref(all_level_data.level1.map(item=> new Soldier(...item))) // 所有人物
+let all_role_locate = ref(all_role.value.map(v => v.role_locate)); // 所有人物坐标点
+// touchMove事件
+function touchMove(move_direct, item) {
+  // 如果不能移动则返回
+  if(!item.itCanMove(all_role_locate.value, move_direct)) return false ;
+  // 坐标处理
+  item.move(move_direct)
+  // 视图处理
+  if (move_direct == 'l') item.left -= grid_size;
+  if (move_direct == 'r') item.left += grid_size;
+  if (move_direct == 't') item.top -= grid_size;
+  if (move_direct == 'b') item.top += grid_size;
+  move_step.value += 1
+  // 判断是否成功
+  const cc_role = (all_role_locate.value.filter((item) => item.name == 'cc'))[0]
+  if (item.isWin(cc_role)) {
+    alert('成功');
+    return false;
+  }
 }
 
-function swipeHandler(move_direct,item){
-  if(item.name == 'cc'){
-    if(item.isWin(item.role_locate)){
-      alert('成功')
-    }
-  }
-  if(item.itCanMove(all_role_locate.value,move_direct)){
-    if(move_direct == 'l'){
-      item.left -= grid_size
-    }
-    if(move_direct == 'r'){
-      item.left += grid_size
-    }
-    if(move_direct == 't'){
-      item.top -= grid_size
-    }
-    if(move_direct == 'b'){
-      item.top += grid_size
-    }
 
-    item.move(move_direct)
-  }else{
-    return false
-  }
+// 获取图片
+function getImage(url) {
+  return new URL(url, import.meta.url).href
 }
 </script>
 
@@ -211,21 +204,7 @@ function swipeHandler(move_direct,item){
   background-color: rgb(201, 177, 147);
   padding-top: 1rem;
   box-sizing: border-box;
-  .role-item {
-    font-family: '楷体';
-    font-size: 0.26rem;
-    line-height: 0.36rem;
-    text-indent: 0.1rem;
-    font-weight: bold;
-    color: goldenrod;
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-    background-position: center center;
-  }
-
-  .cc {
-    color: brown;
-  }
+  touch-action: none; // 禁止页面拖动
 
   #game-title {
     font-weight: bold;
@@ -242,24 +221,39 @@ function swipeHandler(move_direct,item){
     border: 0.1rem solid rgb(0, 0, 0);
     position: relative;
     margin: 0 auto;
-  }
 
-  #role-content {
-    width: 6rem;
-    height: 7.5rem;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+    #role-content {
+      width: 6rem;
+      height: 7.5rem;
+      position: absolute;
+      z-index: 9;
+      top: 0;
+      left: 0;
 
-  #export {
-    width: 3rem;
-    height: 1.5rem;
-    background-color: rgb(201, 177, 147);
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%) translateY(1.5rem);
-    bottom: 0;
-    z-index: 2;
+      .role-item {
+        font-family: '楷体';
+        font-size: 0.26rem;
+        line-height: 0.36rem;
+        text-indent: 0.1rem;
+        font-weight: bold;
+        color: goldenrod;
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        background-position: center center;
+        transition: all 0.2s;
+      }
+    }
+
+    #export {
+      width: 3rem;
+      height: 1.5rem;
+      background-color: rgb(201, 177, 147);
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%) translateY(1.5rem);
+      bottom: 0;
+      z-index: 2;
+    }
   }
-}</style>
+}
+</style>
---
+++ src/views/Error/hrd_level.js
@@ -0,0 +1,31 @@
+const cc_img = "cc.png";
+const cc_cn_name = "曹操";
+const hz_img = "hz.png";
+const hz_cn_name = "黄忠";
+const mc_img = "mc.png";
+const mc_cn_name = "马超";
+const zf_img = "zf.png";
+const zf_cn_name = "张飞";
+const gy_img = "gy.png";
+const gy_cn_name = "关羽";
+const zy_img = "zy.png";
+const zy_cn_name = "赵云";
+
+const xb_img = "xb.png";
+const xb_cn_name = "士";
+
+export default {
+  level1: [
+    [2, 2, [1, 0], cc_img, "cc", cc_cn_name],
+    [1, 2, [0, 0], hz_img, "hz", hz_cn_name],
+    [1, 2, [3, 0], mc_img, "mc", mc_cn_name],
+    [1, 2, [0, 2], zf_img, "zf", zf_cn_name],
+    [2, 1, [1, 2], gy_img, "gy", gy_cn_name],
+    [1, 2, [3, 2], zy_img, "zy", zy_cn_name],
+
+    [1, 1, [0, 4], xb_img, "xb1", xb_cn_name],
+    [1, 1, [1, 3], xb_img, "xb2", xb_cn_name],
+    [1, 1, [2, 3], xb_img, "xb3", xb_cn_name],
+    [1, 1, [3, 4], xb_img, "xb4", xb_cn_name],
+  ],
+};
