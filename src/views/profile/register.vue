12e8fce6870e3d58906eb27466884b062a7c82d8
Wang Ning <AndrewWang0318@outlook.com>
2023-12-24 22:19:00 +0800

    .

--- src/views/profile/register.vue
+++ src/views/profile/register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-register">
     <!-- 注册表单 -->
-    <van-form @submit="onRegisteSubmit" v-show="status == 'registe'">
+    <van-form @submit="onRegisteSubmit">
       <div class="register-content">
         <van-cell-group inset>
           <van-field v-model="registeForm.user_name" name="user_name" label="用户名" placeholder="用户名"
@@ -31,43 +31,22 @@ import { showToast } from "vant"
 import { ref, reactive, onBeforeMount } from 'vue'
 import { useRouter,onBeforeRouteLeave } from 'vue-router';
 import API from "@/server/api"
-
-
-
-
 import $tool from "aw-util"
 const router = useRouter();
 const store = useStore();
-let status = ref("login");
-let autoLogin = ref(true) // 免登录开关
-let loginForm = reactive({
-  user_name: "",
-  user_password: "",
-})
+
+
+
 let registeForm = reactive({
   user_name: "",
   user_password: "",
 })
 
-const onLoginSubmit = (form) => {// 用户登录
-  let params = form
-  API.login(params).then((res) => {
-    showToast(res.data.msg);
-    if (res.data.code == 0) {
-      store.userInfoAction(res.data.user_info) // 存入状态管理
-      // 如果自动登录存在
-      if (autoLogin.value) {
-        $tool.operatCookie("set", "user_info", JSON.stringify(form), 1000 * 60 * 60 * 24); // 存入cookie用于自动登录
-      }
-      router.push("/Home");
-    }
-  });
-}
+
 const onRegisteSubmit = (form) => {// 注册新用户
   API.register(form).then((res) => {
     showToast(res.data.msg);
     if (res.data.code == 0) {
-      status.value = "login";
       registeForm = {
         user_name: "",
         user_password: "",
@@ -76,28 +55,9 @@ const onRegisteSubmit = (form) => {// 注册新用户
     }
   })
 }
-onBeforeMount(() => {
-  // 校验cookie后自动登录
-  let user_cookie = $tool.operatCookie("get", "user_info");
-  if (user_cookie && user_cookie != "{}") {
-    let params = JSON.parse(user_cookie)
-    onLoginSubmit(params);
-  }
-})
-onBeforeRouteLeave((to, from) => {
-  let user_cookie = $tool.operatCookie("get", "user_info");
-  let userInfo = store.userInfo;
-  if (from.path != "/") {
-    if ((user_cookie && user_cookie != "{}") && (userInfo && JSON.stringify(userInfo) != "{}")) {
-      showToast("当前已登录~");
-      return false
-    } else {
-      return true
-    }
-  } else {
-    return true
-  }
-})
+
+
+
 </script>
 
 <style scoped lang="scss">
