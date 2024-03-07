0afeb884846c321facbf234ebfbc2ad4d796b46d
WangNing <AndrewWang0318@outlook.com>
2024-03-07 14:12:53 +0800

    老虎机可以运行

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,7 @@
     <NavBar title="LIGHTHOUSE" color="gray" :leftArrow="false" />
     <div id="area-lottery" class="area-lottery">
       <SlotMachine ref="myLucky" width="7rem" height="4rem" :prizes="prizes" :blocks="blocks"
-        :defaultConfig="defaultConfig" :defaultStyle="defaultStyle" :slots="slots" />
+        :defaultConfig="defaultConfig" :defaultStyle="defaultStyle" :slots="slots" @start="startCallback" />
     </div>
   </div>
 </template>
@@ -11,9 +11,12 @@
 // @start="startCallback" 
 // @end="endCallback" 
 import NavBar from '@/layout/nav-bar.vue';
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { getUrl } from "@/utils/util";
-// getUrl("images/lottery/chicken.png"),
+
+const myLucky = ref("")
+
+// 奖励
 const prizes = [{
   background: '#bac5ee',
   borderRadius: '10px',
@@ -29,38 +32,63 @@ const prizes = [{
   imgs: [{
     width: '60%',
     top: '20%',
-    src: getUrl("images/lottery/chicken.png")
+    src: getUrl("images/lottery/monkey.png")
   }]
-}];
-// const prizes = [
-//   { fonts: [{ text: '6', top: '15%' }] },
-//   { fonts: [{ text: '9', top: '15%' }] },
-// ]
-const blocks = ref([{ padding: '10px', background: '#869cfa' },
-{ padding: '10px', background: '#e9e8fe' },]);
-const buttons = ref([12, 3, 4]);
+},
+{
+  background: '#bac5ee',
+  borderRadius: '10px',
+  imgs: [{
+    width: '60%',
+    top: '20%',
+    src: getUrl("images/lottery/dog.png")
+  }]
+},
+{
+  background: '#bac5ee',
+  borderRadius: '10px',
+  imgs: [{
+    width: '60%',
+    top: '20%',
+    src: getUrl("images/lottery/pig.png")
+  }]
+}
+];
+// 块
+const blocks = ref([{ padding: '8px', background: '#869cfa' },
+{ padding: '6px', background: '#e9e8fe' },]);
+
 const defaultConfig = {
   rowSpacing: '10px',
   colSpacing: '10px'
 }
+// 默认样式
 const defaultStyle = {
   borderRadius: Infinity,
   background: '#bac5ee',
   fontSize: '32px',
   fontColor: '#333'
 }
+// 礼物排序
 const slots = [
-  { order: [1, 0, 1] },
-  { order: [1, 0, 1] },
-  { order: [0, 1, 0] }
+  { order: [0, 1, 2, 3] },
+  { order: [0, 1, 2, 3] },
+  { order: [0, 1, 2, 3] },
+  { order: [0, 1, 2, 3] },
+  // { order: [3, 1, 0] },
 ]
 // blocks buttons prizes
 
 
 
 const startCallback = () => {
-
+  console.log(myLucky.value)
+  myLucky.value.play()
+  myLucky.value.stop([1, 1, 1, 1])
 }
+onMounted(() => {
+  startCallback()
+})
 
 const endCallback = () => {
 
